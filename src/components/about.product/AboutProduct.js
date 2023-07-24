d67b469f75dc25e94cb8edeca3462c5807c03e40
biswajit gahan <dev.infoshare@gmail.com>
2023-07-24 17:28:04 +0530

    about product trial-2

--- src/components/about.product/AboutProduct.js
+++ src/components/about.product/AboutProduct.js
@@ -7,7 +7,7 @@ const AboutProduct = () => {
     <Container>
       {/* <div className="product-background-container"> */}
       <svg className="product-bg" width="1440" height="250" viewBox="0 0 1440 250" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1440 78.5C662.262 250 369.375 2.66667 -1 0V250H1440V78.5Z" fill="#008A91" fill-opacity="0.14" />
+        <path d="M1440 78.5C662.262 250 369.375 2.66667 -1 0V250H1440V78.5Z" fill="#008A91" fillOpacity="0.14" />
       </svg>
       {/* </div> */}
       <div className="product-image-container">
